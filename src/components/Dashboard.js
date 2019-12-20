e4fbe677420568d994acd47880e8a2b0b431ed70
rushabhoswal <rushabh123453@gmail.com>
2019-12-20 05:44:05 +0530

    Get api integrated with props

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import ProjectItem from "./project/ProjectItem";
-import Header from "./layout/Header";
 import ProjectButton from "./project/ProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectAction";
+import PropTypes from "prop-types";
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
+    const { projects } = this.props.project;
+
     return (
       <div className="projects">
         <div className="container">
@@ -15,7 +23,9 @@ export default class Dashboard extends Component {
               <ProjectButton />
               <br />
               <hr />
-              <ProjectItem />
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
@@ -23,4 +33,16 @@ export default class Dashboard extends Component {
     );
   }
 }
-//export default Dashboard;
+const mapStateToProp = state => ({
+  project: state.project
+});
+
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+};
+
+export default connect(
+  mapStateToProp,
+  { getProjects }
+)(Dashboard);
